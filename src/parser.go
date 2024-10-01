5b68eefa11c05a5723e7f88bb46b2ed98488b52a
Aditya Sharma <adityaksharma@gmail.com>
2024-10-01 13:27:59 -0700

    fix: Correct SiteTitle assignment to use Sitename from metadata

--- src/parser.go
+++ src/parser.go
@@ -582,7 +582,7 @@ func createFeedResponse(feed *gofeed.Feed, url string, metaData link2json.MetaDa
 		feedType = "article"
 	}
 
-	SiteTitle := metaData.Title
+	SiteTitle := metaData.Sitename
 	if SiteTitle == "" {
 		SiteTitle = feed.Title
 	}
